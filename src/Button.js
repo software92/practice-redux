3559aae8c6d0cb3b3ad1ca3e03436e186b38f089
jsw <csw9201@naver.com>
2023-10-30 17:22:56 +0900

    feat: redux-toolkit(createSlice)

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import store from './store';
+import store, { add, minus } from './store';
 
 const Button = ({ label, type }) => {
   const handleClick = () => {
-    store.dispatch({ type });
+    // store.dispatch({ type });
+    if (type === 'ADD') {
+      store.dispatch(add());
+    } else {
+      store.dispatch(minus());
+    }
   };
   return <button onClick={handleClick}>{label}</button>;
 };
--- src/store.js
+++ src/store.js
@@ -1,29 +1,43 @@
-import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
+import { configureStore, createAction, createReducer, createSlice } from '@reduxjs/toolkit';
 
-const addAction = createAction('ADD');
-const minusAction = createAction('MINUS');
+// const addAction = createAction('ADD');
+// const minusAction = createAction('MINUS');
 
-// // deprecated
-// const reducer = createReducer(0, {
-//   [addAction]: (state, action) => {
-//     return state + 1;
-//   },
-//   [minusAction]: (state, action) => {
-//     return state - 1;
-//   },
-// });
+// // // deprecated
+// // const reducer = createReducer(0, {
+// //   [addAction]: (state, action) => {
+// //     return state + 1;
+// //   },
+// //   [minusAction]: (state, action) => {
+// //     return state - 1;
+// //   },
+// // });
 
-// builder callback
-const reducer = createReducer({ count: 0 }, builder =>
-  builder
-    .addCase(addAction, (state, action) => {
+// // builder callback
+// const reducer = createReducer({ count: 0 }, builder =>
+//   builder
+//     .addCase(addAction, (state, action) => {
+//       state.count++;
+//     })
+//     .addCase(minusAction, (state, action) => {
+//       state.count--;
+//     })
+// );
+
+const todo = createSlice({
+  name: 'todo-reducer',
+  initialState: { count: 0 },
+  reducers: {
+    add: (state, action) => {
       state.count++;
-    })
-    .addCase(minusAction, (state, action) => {
+    },
+    minus: (state, action) => {
       state.count--;
-    })
-);
+    },
+  },
+});
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: todo.reducer });
 
+export const { add, minus } = todo.actions;
 export default store;
